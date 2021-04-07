7cb6aa23f46c13481ad4c6107b9ee021ce063447
Alberto <albertitovalls97@gmail.com>
2021-04-07 16:08:25 +0200

    botones construccion

--- Assets/_CrystalGuardians/HUD/Scripts/ConstruirEstructuras.cs
+++ Assets/_CrystalGuardians/HUD/Scripts/ConstruirEstructuras.cs
@@ -28,7 +28,7 @@ public class ConstruirEstructuras : MonoBehaviour
         // coger los botones y deshabilitar los que requieran un nivel superior al del castillo 
         // y deshabilitar los que no puedes construir por recursos
 
-        GameManager.Instance.Oro = 1350;
+        GameManager.Instance.Oro = 2350;
         
 
         Debug.Log("start spawn" + GameManager.nivelMinimoCastilloCasaHechizos);
@@ -43,17 +43,19 @@ public class ConstruirEstructuras : MonoBehaviour
 
         GameManager g = GameManager.Instance;
         Castillo castillo = g.castillo.GetComponent<Castillo>();
-        
+
+        btnCasaHechizos = GameObject.Find("btnCasaHechizos").GetComponent<Button>();
+
         // comprobar nivel y recursos
-        if(castillo.nivelActual >= GameManager.nivelMinimoCastilloCasaHechizos && GameManager.Instance.Oro >= GameManager.costeConstruirCasaHechizos)
+        if (castillo.nivelActual >= GameManager.nivelMinimoCastilloCasaHechizos && GameManager.Instance.Oro >= GameManager.costeConstruirCasaHechizos)
         {
-            
+            btnCasaHechizos.interactable = true;
             GameManager.Instance.Oro = GameManager.Instance.Oro - GameManager.costeConstruirCasaHechizos;
             Instantiate(CasaDeHechizos_blueprint);
         }
         else
         {
-            //btnCasaHechizos.interactable = false;
+            btnCasaHechizos.interactable = false;
         }
 
         
@@ -63,41 +65,47 @@ public class ConstruirEstructuras : MonoBehaviour
 
     public void spawn_CuartelUnidades_blueprint()
     {
-        
+         
+
         GameManager g = GameManager.Instance;
         Castillo castillo = g.castillo.GetComponent<Castillo>();
+        
+        btnCuartel = GameObject.Find("btnCuartel").GetComponent<Button>();
+        
+
 
         // comprobar nivel y recursos
         if (castillo.nivelActual >= GameManager.nivelMinimoCastilloCuartel && GameManager.Instance.Oro >= GameManager.costeConstruirCuartel)
         {
-            
+            btnCuartel.interactable = true;
             GameManager.Instance.Oro = GameManager.Instance.Oro - GameManager.costeConstruirCuartel;
             Instantiate(CuartelUnidades_blueprint);
         }
         else
         {
-            //btnCuartel.interactable = false;
+            btnCuartel.interactable = false;
         }
 
-       
+
+
     }
     public void spawn_Mina()
     {
         
         GameManager g = GameManager.Instance;
         Castillo castillo = g.castillo.GetComponent<Castillo>();
-      
 
+        btnMina = GameObject.Find("btnMina").GetComponent<Button>();
         // comprobar nivel y recursos
         if (castillo.nivelActual >= GameManager.nivelMinimoCastilloMina && GameManager.Instance.Oro >= GameManager.costeConstruirMina)
         {
-           
+            btnMina.interactable = true;
             GameManager.Instance.Oro = GameManager.Instance.Oro - GameManager.costeConstruirMina;
             Instantiate(Mina_blueprint);
         }
         else
         {
-           // btnMina.interactable = false;
+           btnMina.interactable = false;
         }
        
 
@@ -108,16 +116,18 @@ public class ConstruirEstructuras : MonoBehaviour
         GameManager g = GameManager.Instance;
         Castillo castillo = g.castillo.GetComponent<Castillo>();
 
+        btnExtractor = GameObject.Find("btnExtractor").GetComponent<Button>();
+
         // comprobar nivel y recursos
         if (castillo.nivelActual >= GameManager.nivelMinimoCastilloExtractor && GameManager.Instance.Oro >= GameManager.costeConstruirExtractor)
         {
-            
+            btnExtractor.interactable = true;
             GameManager.Instance.Oro = GameManager.Instance.Oro - GameManager.costeConstruirExtractor;
             Instantiate(Extractor_blueprint);
         }
         else
         {
-            //btnExtractor.interactable = false;
+            btnExtractor.interactable = false;
         }
        
 
@@ -129,16 +139,18 @@ public class ConstruirEstructuras : MonoBehaviour
         GameManager g = GameManager.Instance;
         Castillo castillo = g.castillo.GetComponent<Castillo>();
 
+        btnMuro = GameObject.Find("btnMuro").GetComponent<Button>();
+
         // comprobar nivel y recursos
         if (castillo.nivelActual >= GameManager.nivelMinimoCastilloMuros && GameManager.Instance.Oro >= GameManager.costeConstruirMuro)
         {
-            
+            btnMuro.interactable = true;
             GameManager.Instance.Oro = GameManager.Instance.Oro - GameManager.costeConstruirCasaHechizos;
             Instantiate(Muro_blueprint);
         }
         else
         {
-           // btnMuro.interactable = false;
+           btnMuro.interactable = false;
         }
        
         
@@ -151,16 +163,18 @@ public class ConstruirEstructuras : MonoBehaviour
         GameManager g = GameManager.Instance;
         Castillo castillo = g.castillo.GetComponent<Castillo>();
 
+        btnTorre = GameObject.Find("btnTorre").GetComponent<Button>();
+
         // comprobar nivel y recursos
         if (castillo.nivelActual >= GameManager.nivelMinimoCastilloTorre && GameManager.Instance.Oro >= GameManager.costeConstruirTorre)
         {
-            
+            btnTorre.interactable = true;
             GameManager.Instance.Oro = GameManager.Instance.Oro - GameManager.costeConstruirTorre;
             Instantiate(Torre_blueprint);
         }
         else
         {
-            //btnTorre.interactable = false;
+            btnTorre.interactable = false;
         }
         
 
@@ -171,16 +185,17 @@ public class ConstruirEstructuras : MonoBehaviour
         GameManager g = GameManager.Instance;
         Castillo castillo = g.castillo.GetComponent<Castillo>();
 
+        btnCuartel = GameObject.Find("btnTrampa").GetComponent<Button>();
         // comprobar nivel y recursos
         if (castillo.nivelActual >= GameManager.nivelMinimoCastilloTrampa && GameManager.Instance.Oro >= GameManager.costeConstruirTrampa)
         {
-            
+            btnTrampa.interactable = true;
             GameManager.Instance.Oro = GameManager.Instance.Oro - GameManager.costeConstruirTrampa;
             Instantiate(Trampa_blueprint);
         }
         else
         {
-            //btnTrampa.interactable = false;
+            btnTrampa.interactable = false;
         }
         
 

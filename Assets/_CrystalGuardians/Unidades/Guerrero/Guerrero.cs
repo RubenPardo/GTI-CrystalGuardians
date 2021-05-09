96ae8180f4bc03fc57254dddb4f008d26ad2ceb0
Alberto <albertitovalls97@gmail.com>
2021-05-09 21:57:00 +0200

    Implementacion guerrero y casa hechizos

--- Assets/_CrystalGuardians/Unidades/Guerrero/Guerrero.cs
+++ Assets/_CrystalGuardians/Unidades/Guerrero/Guerrero.cs
@@ -6,7 +6,9 @@ using System.Linq;
 
 public class Guerrero : Aliado
 {
-
+    public GameObject prefabLvl1;
+    public GameObject prefabLvl2;
+    public GameObject prefabLvl3;
     public static float mejoraDanyo = 1f;//mejora de aldea
                                         
 
@@ -18,7 +20,31 @@ public class Guerrero : Aliado
 
         base.mejoraDanyo = Guerrero.mejoraDanyo;
         base.Update();
+        comprobarCambiarPrefab();
+    }
+    private void comprobarCambiarPrefab()
+    {
+        /*if (nivelActual > 0) {
+            if (nivelActual == 1)
+            {
+                // prefab nivel 2
+                prefabLvl1.SetActive(false);
+                prefabLvl2.SetActive(true);
+
+
+            }
+            else
+            {
+                // prefab nivel 3
+                prefabLvl2.SetActive(false);
+                prefabLvl3.SetActive(true);
+               
+            }
+        }*/
+    }
+
     }
 
 
-}
+
+

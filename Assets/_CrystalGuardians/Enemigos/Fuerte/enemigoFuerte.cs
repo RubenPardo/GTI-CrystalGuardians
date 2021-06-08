ef5732c149b4a161032e82046ba7df5ee941a3e8
Ruben <rubenpardocasanova@gmail.com>
2021-06-08 17:41:45 +0200

    Merge Errores combate develop

--- Assets/_CrystalGuardians/Enemigos/Fuerte/enemigoFuerte.cs
+++ Assets/_CrystalGuardians/Enemigos/Fuerte/enemigoFuerte.cs
@@ -9,36 +9,17 @@ public class enemigoFuerte : EnemigoScript
     public AnimacionController AnimacionController;
 
    
-
-    protected override void Start()
-    {
-    
-        base.Start();
-    }
-
     // Update is called once per frame
     protected override void Update()
     {
-        base.Update();
         AnimacionController.damage = danyoPorNivel[nivelActual];
     }
 
-    override public void attack()
-    {
-        /*Metodo que invoca el ataque fuerte del enemigo
-        //El ataque se produce al terminar la animacion
-        */
-    }
+  
 
     public override List<GameObject> getPossibleTargets()
     {
         return GameManager.listaEstructurasEnJuego;
     }
 
-    protected override void OnDestroy()
-    {
-       
-        GameManager.Instance.listaEnemigosRonda.Remove(gameObject);
-        base.OnDestroy();
-    }
 }

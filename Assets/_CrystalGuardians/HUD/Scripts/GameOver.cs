c5f038732743cca0e21b6dc320341df746fe48a2
JuanC99 <juancarloshr123@gmail.com>
2021-06-12 21:52:29 +0200

    Pantalla Game Over Completa

--- Assets/_CrystalGuardians/HUD/Scripts/GameOver.cs
+++ Assets/_CrystalGuardians/HUD/Scripts/GameOver.cs
@@ -8,9 +8,26 @@ public class GameOver : MonoBehaviour
 {
 
     public Text textOro;
+    public Text textObsidium;
+    public Text textUnidadesGeneradas;
+    public Text textEnemigosEliminados;
+    public Text textHechizosLanzados;
+    public Text textEstructurasConstruidas;
+    public Text textTiempoDeJuego;
+    public Text textRondaMasAlta;
 
     public void UpdateStats()
     {
+        textUnidadesGeneradas.text = GameManager.Instance.UnidadesAliadasTotalesGeneradas.ToString();
+        textEnemigosEliminados.text = GameManager.Instance.EnemigosTotalesEliminados.ToString();
+        textHechizosLanzados.text = GameManager.Instance.HechizosTotalesLanzados.ToString();
+        textEstructurasConstruidas.text = GameManager.Instance.EstructurasTotalesConstruidas.ToString();
+        textRondaMasAlta.text = GameManager.Instance.RondaMaximaAlcanzada.ToString();
+
+        float time = Time.timeSinceLevelLoad;
+        float seconds = Mathf.FloorToInt(time % 60);
+        float minutes = Mathf.FloorToInt(time / 60);
+        textTiempoDeJuego.text = string.Format("{0:00}:{1:00}", minutes, seconds);
 
         if (GameManager.Instance.OroTotalGenerado >= 1000 && GameManager.Instance.OroTotalGenerado < 1000000)
         {
@@ -26,6 +43,22 @@ public class GameOver : MonoBehaviour
         {
             textOro.text = GameManager.Instance.OroTotalGenerado.ToString("f0");
         }
+
+        if (GameManager.Instance.ObsidiumTotalGenerado >= 1000 && GameManager.Instance.ObsidiumTotalGenerado < 1000000)
+        {
+
+            float cantidadRedondeada = GameManager.Instance.ObsidiumTotalGenerado / 1000;
+            textObsidium.text = cantidadRedondeada.ToString("f2") + "k";
+        }
+        else if (GameManager.Instance.ObsidiumTotalGenerado >= 1000000)
+        {
+            float cantidadRedondeada = GameManager.Instance.ObsidiumTotalGenerado / 1000000;
+            textObsidium.text = cantidadRedondeada.ToString("f2") + "M";
+        }
+        else
+        {
+            textObsidium.text = GameManager.Instance.ObsidiumTotalGenerado.ToString("f0");
+        }
     }
     public void ReloadScene()
     {
@@ -34,6 +67,6 @@ public class GameOver : MonoBehaviour
 
     public void BackToMainMenu()
     {
-
+        SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
     }
 }
